b4d1c0e012af906b063e3fa003019d5f85d0a7f1
Graham Barber <grahammbarber@gmail.com>
2017-10-07 11:18:28 -0700

    Deleted presentation assets, updated slides

--- slideshow/presentation/index.js
+++ slideshow/presentation/index.js
@@ -1,5 +1,5 @@
 // Import React
-import React from "react";
+import React from 'react'
 
 // Import Spectacle Core tags
 import {
@@ -11,76 +11,90 @@ import {
   List,
   Quote,
   Slide,
-  Text
-} from "spectacle";
+  Text,
+  CodePane
+} from 'spectacle'
 
 // Import image preloader util
-import preloader from "spectacle/lib/utils/preloader";
+import preloader from 'spectacle/lib/utils/preloader'
 
 // Import theme
-import createTheme from "spectacle/lib/themes/default";
+import createTheme from 'spectacle/lib/themes/default'
 
 // Require CSS
-require("normalize.css");
-require("spectacle/lib/themes/default/index.css");
+require('normalize.css')
+require('spectacle/lib/themes/default/index.css')
 
-
-const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
-};
-
-preloader(images);
-
-const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quartenary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const theme = createTheme(
+  {
+    primary: 'white',
+    secondary: '#1F2022',
+    tertiary: '#03A9FC',
+    quartenary: '#CECECE'
+  },
+  {
+    primary: 'Montserrat',
+    secondary: 'Helvetica'
+  }
+)
 
 export default class Presentation extends React.Component {
-  render() {
+  render () {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck
+        transition={['zoom', 'slide']}
+        transitionDuration={500}
+        theme={theme}
+      >
+        <Slide transition={['zoom']} bgColor='primary'>
+          <Heading size={1} fit caps lineHeight={1} textColor='secondary'>
+            ES Modules
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin='10px 0 0' textColor='tertiary' size={1} fit bold>
+            Something something standards
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+        <Slide transition={['fade']} bgColor='primary' textColor='tertiary'>
+          <Heading size={6} textColor='secondary' caps>
+            Packaging JavaScript
+          </Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <ListItem>IIFEs</ListItem>
+            <ListItem>CommonJS</ListItem>
+            <ListItem>Bundling</ListItem>
+            <ListItem>ES Modules</ListItem>
           </List>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+        <Slide transition={['fade']} bgColor='primary' textColor='tertiary'>
+          <Heading size={1} textColor='secondary'>
+            IIFEs
+          </Heading>
+        </Slide>
+        <Slide transition={['fade']} bgColor='primary' textColor='tertiary'>
+          <Heading size={1} textColor='secondary' caps>
+            Bundling
+          </Heading>
+          <List>
+            <ListItem>webpack</ListItem>
+            <ListItem>Rollup</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={['fade']} bgColor='secondary' textColor='tertiary'>
           <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
+            <Quote>"webpack is scary"</Quote>
+            <Cite>Probably You</Cite>
           </BlockQuote>
         </Slide>
+        <Slide transition={['fade']} bgColor='primary' textColor='tertiary'>
+          <Heading size={1} textColor='secondary' caps>
+            The Future
+          </Heading>
+          <List>
+            <ListItem>Native ES Modules</ListItem>
+            <ListItem>HTTP2</ListItem>
+          </List>
+        </Slide>
       </Deck>
-    );
+    )
   }
 }
